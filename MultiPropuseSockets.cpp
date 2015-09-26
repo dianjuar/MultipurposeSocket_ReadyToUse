622312af9ae478299ce84bc07f1b4800f2298b14
dianjuar <dianjuar@gmail.com>
2015-09-26 06:27:09 -0430

    initial commit

---
+++ MultiPropuseSockets.cpp
@@ -0,0 +1,116 @@
+#include "MultiPropuseSockets.h"
+
+using namespace Network;
+
+
+Base::DataSend::DataSend(QTcpSocket *socket)
+{
+    this->socket = socket;
+}
+
+void Base::DataSend::write(QString s)
+{
+    socket->write( s.toUtf8() );
+    socket->flush();
+}
+/////////////////////////////////////////////////////////
+Base::DataReceiver::DataReceiver(QTcpSocket *socket)
+{
+    this->socket = socket;
+}
+
+void Base::DataReceiver::readyRead()
+{
+    qDebug()<<("*********Reading**********");
+    QString s( socket->readAll() );
+    qDebug()<<s;
+    qDebug()<<("********Analizing***********");
+
+    dataAnalizer(s);
+}
+/////////////////////////////////////////////////////////
+Client::Client(QString host, int port):
+    Base::DataSend(&socket),
+    Base::DataReceiver(&socket)
+{
+    connected_B = false;
+    this->host = host;
+    this->port = port;
+}
+
+void Client::connectToHost()
+{
+    socket.connectToHost(host, port);
+
+    connected_B = socket.waitForConnected(3000);
+
+    if(connected_B)//connected
+    {
+        connect(&socket, SIGNAL(connected()), this, SLOT(connected()));
+        connect(&socket, SIGNAL(disconnected()), this, SLOT(disconnected()));
+        connect(&socket, SIGNAL(bytesWritten(qint64)), this, SLOT(bytesWritten(qint64)));
+        connect(&socket, SIGNAL(readyRead()), this, SLOT(readyRead()));
+    }
+    else
+    {
+        qDebug()<<"ERROR connecting to-> "<<host<<":"<<port;
+    }
+}
+
+void Client::connected()
+{
+    qDebug()<<"CONECTED to -> "<<host<<":"<<port;
+}
+
+void Client::disconnected()
+{
+     qDebug()<<"DISCONECTED to -> "<<host<<":"<<port;
+}
+
+void Client::bytesWritten(qint64 bytes)
+{
+    qDebug()<<"Bytes written"<<bytes;
+}
+/////////////////////////////////////////////////////////////////////////
+ServerSimple::ServerSimple(QHostAddress adds, int port)
+{
+    server = new QTcpServer(this);
+    connect(server,SIGNAL(newConnection()), this, SLOT(newConnection()) );
+
+    if( !server->listen(adds,port) )
+    {
+        qDebug()<<"Server couldn't start";
+    }
+    else
+    {
+       qDebug()<<"Server started. Listening petitions at"<<adds<<":"<<port;
+    }
+}
+
+void ServerSimple::newConnection()
+{
+    socket = server->nextPendingConnection();
+
+    set_socket_DR(socket);
+    set_socket_DS(socket);
+
+    connect(socket, SIGNAL(connected()), this, SLOT(connected()));
+    connect(socket, SIGNAL(disconnected()), this, SLOT(disconnected()));
+    connect(socket, SIGNAL(bytesWritten(qint64)), this, SLOT(bytesWritten(qint64)));
+    connect(socket, SIGNAL(readyRead()), this, SLOT(readyRead()));
+}
+
+void ServerSimple::connected()
+{
+    qDebug()<<"CONECTED to -> "<<socket->localAddress()<<":"<<socket->peerPort();
+}
+
+void ServerSimple::disconnected()
+{
+     qDebug()<<"DISCONECTED to -> "<<socket->localAddress()<<":"<<socket->peerPort();
+}
+
+void ServerSimple::bytesWritten(qint64 bytes)
+{
+    qDebug()<<"Bytes written"<<bytes<<"in"<<socket->localAddress()<<":"<<socket->peerPort();
+}
---
+++ MultiPropuseSockets.h
@@ -0,0 +1,114 @@
+#ifndef NETWORKING_H
+#define NETWORKING_H
+
+#include <QTcpSocket>
+#include <QTcpServer>
+#include <QAbstractSocket>
+
+#include <QDebug>
+#include <QByteArray>
+#include <QString>
+
+namespace Network
+{
+    namespace Base
+    {
+        class DataReceiver: public QObject
+        {
+            Q_OBJECT
+        private:
+            QTcpSocket *socket;
+
+            virtual void dataAnalizer(QString msj){}
+
+        public slots:
+            void readyRead();
+
+        public:
+            DataReceiver(QTcpSocket *socket);
+            DataReceiver(){ }
+
+            //setters
+            void set_socket_DR( QTcpSocket *s ) { this->socket = s; }
+
+            void run();
+        };
+        ////////////////////////////////////////////
+        class DataSend
+        {
+        private:
+            QTcpSocket *socket;
+
+        public:
+            DataSend(QTcpSocket *socket);
+            DataSend(){}
+
+            //setters
+            void set_socket_DS( QTcpSocket *s ) { this->socket = s; }
+
+            void write(QString s);
+        };
+    }
+
+    ////////////////////////////////////////////
+    class Client: public Network::Base::DataReceiver, public Network::Base::DataSend
+    {
+        Q_OBJECT
+    private:
+       QTcpSocket socket;
+
+    protected:
+       QString host;
+       int port;
+       bool connected_B;
+
+    public:
+       Client(QString host, int port);
+       void connectToHost();
+
+       //getters
+       bool isConnected()   { return connected_B; }
+       QString get_host()   { return host; }
+       int get_port()       { return port; }
+
+    signals:
+
+    public slots:
+       void	connected();
+       void	disconnected();
+       void	bytesWritten(qint64 bytes);
+    };
+    ///////////////////////////////////////////
+    /// \brief The ServerSimple class
+    /// Only works for one petition... for now.
+    class ServerSimple: public Network::Base::DataReceiver, public Network::Base::DataSend
+    {
+        Q_OBJECT
+    private:
+       QTcpSocket *socket;
+       QTcpServer *server;
+
+    protected:
+       QHostAddress adds;
+       int port;
+
+    public:
+       ServerSimple(QHostAddress adds, int port);
+
+       //getters
+       QHostAddress get_host()   { return adds; }
+       int get_port()            { return port; }
+
+    signals:
+
+    public slots:
+       void newConnection();
+       void	connected();
+       void	disconnected();
+       void	bytesWritten(qint64 bytes);
+
+    };
+
+}
+
+#endif // NETWORKING_H
